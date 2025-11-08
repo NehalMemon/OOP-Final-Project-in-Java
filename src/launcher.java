46a4f11e430e4ffb35b32e061b567512f72ad49a
NehalMemon <nehalmemon73@gmail.com>
2025-11-08 18:44:36 +0500

    setup folder structure

---
+++ src/launcher.java
@@ -0,0 +1,9 @@
+import src.ui.loginFrame;
+public class launcher {
+    public static void main(String[] args){
+        System.out.println("App is running");
+        new loginFrame();
+
+    }
+    
+}
---
+++ src/ui/loginFrame.java
@@ -0,0 +1,12 @@
+package src.ui;
+
+import javax.swing.*;
+
+public class loginFrame extends JFrame{
+    public loginFrame(){
+        this.setTitle("Smart Student Manager - Login");
+        this.setSize(300,200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+        this.setVisible(true);
+    }
+}
